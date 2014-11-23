46edda4838a3ed41b6a00ac39fe1f896e22dd220
MonkeyTime <gitmonkeytime@gmail.com>
2014-11-23 17:34:45 +0100

    update serialized array of valid languages

--- master/BattleNet/public/cron.php
+++ master/BattleNet/public/cron.php
@@ -4,7 +4,7 @@ define('ROOT_PATH', realpath(dirname(__DIR__)));
 define('APP_PATH', ROOT_PATH . '/application');
 define('PUBLIC_PATH', ROOT_PATH . '/public');
 
-define('API_LOCALES', 'a:13:{i:0;s:5:"en_US";i:1;s:5:"pt_BR";i:2;s:5:"es_MX";i:3;s:5:"en_GB";i:4;s:5:"de_DE";i:5;s:5:"es_ES";i:6;s:5:"fr_FR";i:7;s:5:"it_IT";i:8;s:5:"pl_PL";i:9;s:5:"pt_PT";i:10;s:5:"ru_RU";i:11;s:5:"zh_TW";i:12;s:5:"ko_KR";i:13;s:5:"zh_CN";}');
+define('API_LOCALES', 'a:14:{i:0;s:5:"en_US";i:1;s:5:"pt_BR";i:2;s:5:"es_MX";i:3;s:5:"en_GB";i:4;s:5:"de_DE";i:5;s:5:"es_ES";i:6;s:5:"fr_FR";i:7;s:5:"it_IT";i:8;s:5:"pl_PL";i:9;s:5:"pt_PT";i:10;s:5:"ru_RU";i:11;s:5:"zh_TW";i:12;s:5:"ko_KR";i:13;s:5:"zh_CN";}');
 define('API_LINKS', 'a:14:{s:5:"en_US";s:26:"https://us.api.battle.net/";s:5:"pt_BR";s:26:"https://us.api.battle.net/";s:5:"es_MX";s:26:"https://us.api.battle.net/";s:5:"en_GB";s:26:"https://eu.api.battle.net/";s:5:"de_DE";s:26:"https://eu.api.battle.net/";s:5:"es_ES";s:26:"https://eu.api.battle.net/";s:5:"fr_FR";s:26:"https://eu.api.battle.net/";s:5:"it_IT";s:26:"https://eu.api.battle.net/";s:5:"pl_PL";s:26:"https://eu.api.battle.net/";s:5:"pt_PT";s:26:"https://eu.api.battle.net/";s:5:"ru_RU";s:26:"https://eu.api.battle.net/";s:5:"zh_TW";s:26:"https://tw.api.battle.net/";s:5:"ko_KR";s:26:"https://kr.api.battle.net/";s:5:"zh_CN";s:29:"https://www.battlenet.com.cn/";}');
 
 define('API_KEY', 'YOUR API KEY'); //https://dev.battle.net/
